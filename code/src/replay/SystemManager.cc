cd80a20af2686e3010b9910cf9330446570b0052
Hao Wu <haowu@cs.utexas.edu>
2011-06-27 13:10:16 -0500

    least system management

--- code/src/replay/SystemManager.cc
+++ code/src/replay/SystemManager.cc
@@ -1,8 +1,30 @@
 // Author: Hao Wu <haowu@cs.utexas.edu>
 
+#include <replay/ProcessManager.h>
 #include <replay/SystemManager.h>
 using namespace std;
 
 int SystemManager::execAll()
 {
+    Vector<Vector<String> >::iterator command_pt;
+    for (command_pt = commands.begin(); command_pt < commands.end(); ++command_pt)
+    {
+        // Fork a process for the process
+        pid_t pid = fork();
+        // If the fork fails
+        if (pid == -1)
+        {
+            LOG("fork fails when trying to run %s", (*command_pt)[0].c_str());
+            return -1;
+        }
+
+        // If it is the child process
+        if (pid == 0)
+        {
+            ProcessManager manager(*command_pt, syscallList);
+            manager.replay();
+        }
+    }
+    return 0;
 }
+
--- code/src/replay/SystemManager.h
+++ code/src/replay/SystemManager.h
@@ -3,6 +3,8 @@
 #ifndef __REPLAY_SYSTEMMANAGER_H__
 #define __REPLAY_SYSTEMMANAGER_H__
 
+#include <common/common.h>
+
 // This class is used to manager a *system*, specifically, to manage different processes.
 class SystemManager
 {
@@ -10,6 +12,10 @@ class SystemManager
         // Execute all the processes managed by the manager
         int execAll();
     private:
+        // All the commands in the system manager
+        Vector<Vector<String> > commands;
+        // The system call list;
+        SystemCallList syscallList;
 };
 
 #endif //__REPLAY_SYSTEMMANAGER_H__
