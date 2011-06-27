82997247215850eaa4f90b734973d19419a6ff66
Hao Wu <haowu@cs.utexas.edu>
2011-06-27 12:02:26 -0500

    system manager api

---
+++ code/src/replay/SystemManager.cc
@@ -0,0 +1,8 @@
+// Author: Hao Wu <haowu@cs.utexas.edu>
+
+#include <replay/SystemManager.h>
+using namespace std;
+
+int SystemManager::execAll()
+{
+}
---
+++ code/src/replay/SystemManager.h
@@ -0,0 +1,15 @@
+// Author: Hao Wu<haowu@cs.utexas.edu>
+
+#ifndef __REPLAY_SYSTEMMANAGER_H__
+#define __REPLAY_SYSTEMMANAGER_H__
+
+// This class is used to manager a *system*, specifically, to manage different processes.
+class SystemManager
+{
+    public:
+        // Execute all the processes managed by the manager
+        int execAll();
+    private:
+};
+
+#endif //__REPLAY_SYSTEMMANAGER_H__
