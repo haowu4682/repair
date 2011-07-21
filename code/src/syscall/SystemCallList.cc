754c10acb064627e152b53a4801535042253709d
Hao Wu <haowu@cs.utexas.edu>
2011-07-21 15:08:50 -0500

    add debugging info

--- code/src/syscall/SystemCallList.cc
+++ code/src/syscall/SystemCallList.cc
@@ -13,7 +13,7 @@ SystemCall SystemCallList::search(SystemCall &syscall)
     if (pidManager != NULL)
     {
         pid_t oldPid = pidManager->getOld(newPid);
-        //LOG("%d", oldPid);
+        LOG("%d", oldPid);
         SyscallMapType::iterator it = syscallMap.find(oldPid);
         if (it != syscallMap.end())
         {
