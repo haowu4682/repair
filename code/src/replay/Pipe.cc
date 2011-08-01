f2784d678f7fa22bc71a36126804c249632078f9
Hao Wu <haowu@cs.utexas.edu>
2011-08-01 14:14:38 -0500

    some pipe file

--- code/src/replay/Pipe.cc
+++ code/src/replay/Pipe.cc
@@ -1,9 +1,22 @@
 // Author: Hao Wu <haowu@cs.utexas.edu>
 
+#include <unistd.h>
+
 #include <replay/Pipe.h>
+
+Pipe::Pipe(const int *fileDes, long seqNum, FDManager *fdManager)
+{
+    setFileDes(fileDes);
+    setSeqNum(seqNum);
+    setFDManager(fdManager);
+}
 
 int Pipe::exec()
 {
+    int newFileDes[2];
+    newFileDes[0] = fdManager->oldToNew(fileDes[0], seqNum);
+    newFileDes[1] = fdManager->oldToNew(fileDes[1], seqNum);
+    pipe(newFileDes);
     return 0;
 }
 
--- code/src/replay/Pipe.h
+++ code/src/replay/Pipe.h
@@ -4,10 +4,23 @@
 #define __REPLAY_PIPE_H__
 
 #include <replay/Actor.h>
+#include <replay/FDManager.h>
 
 class Pipe : public Actor
 {
-    virtual int exec();
+    public:
+        virtual int exec();
+        Pipe(const int *fileDes, long seqNum, FDManager *fdManager);
+        const int *getFileDes() const { return fileDes; }
+        void setFileDes(const int *fileDes) { this->fileDes[0] = fileDes[0]; this->fileDes[1] = fileDes[1]; }
+        FDManager *getFDManager() { return fdManager; }
+        void setFDManager(FDManager *manager) { fdManager = manager; }
+        long getSeqNum() { return seqNum; }
+        void setSeqNum(long seqNum) { this->seqNum = seqNum; }
+    private:
+        int fileDes[2];
+        FDManager *fdManager;
+        long seqNum;
 };
 
 #endif //__REPLAY_PIPE_H__
