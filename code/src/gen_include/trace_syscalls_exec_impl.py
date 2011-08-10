7f38106525294de7174e8c0c2bd238decb4b7a57
Hao Wu <haowu@cs.utexas.edu>
2011-08-10 14:27:13 -0500

    add files not in the repo

---
+++ code/src/gen_include/trace_syscalls_exec_impl.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from syscall import syscalls
+from ctypes import sizeof, Structure
+
+# generate C definitions for syscall exec's
+def print_syscalls_exec():
+    for i, sc in enumerate(syscalls):
+        if not sc:
+            continue
+        assert(i == sc.nr)
+        print 'SYSCALL_(%s)' % sc.name
+        print '{'
+        print '    return 0;'
+        print '}'
+        print ''
+
+if __name__ == "__main__":
+    print_syscalls_exec()
+
